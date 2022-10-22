ce21c7234b3ea96353f3e6bae900b95d389c5bda
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-22 19:40:42 +0530

    added streamBuilder

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:chat_app/pages/home_page.dart';
+import 'package:chat_app/shared/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +16,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.hasData) {
+              return const HomePage();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Some Error Occured!!'),
+              );
+            }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          }
+          return HomePage();
+        }),
+      ),
     );
   }
 }
